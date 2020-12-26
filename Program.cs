3ad23fee24e557851702bdbb72076fd914134d7f
PilipenkoEugene <70794926+PilipenkoEugene@users.noreply.github.com>
2020-12-26 10:52:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Lab10Dll;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mat1 = new Matrix();
+            var mat2 = new Matrix();
+
+            mat1.LoadMatrix("mat1.txt");
+            mat2.LoadMatrix("mat2.txt");
+
+            Console.WriteLine("Произведение диагонали первой матрицы: " + mat1.DiagonalProduct());
+            Console.WriteLine("Сумма элементов второй матрицы: " + mat2.SumOfAllElements());
+
+            Console.ReadKey();
+        }
+    }
+}
